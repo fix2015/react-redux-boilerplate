08b6ac14872d1bfc3004ce49b96b1869fba0f626
vitaliy <vitalya@eyespy360.com>
2020-04-10 13:01:41 +0300

    redux logger

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
+import {compose, createStore} from 'redux'
 import { Provider } from 'react-redux'
 import App from './components/App'
 import rootReducer from './reducers'
+import DevTools from './containers/DevTools';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer)
+const enhancer = compose(
+    // DevTools.instrument(),
+    composeWithDevTools()
+);
+
+const store = createStore(rootReducer, enhancer)
 
 render(
     <Provider store={store}>
