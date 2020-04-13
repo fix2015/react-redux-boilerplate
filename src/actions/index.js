54e39aaf6ef9aadfe3fca5a027f6f41cc951df97
vitaliy <vitalya@eyespy360.com>
2020-04-13 14:19:38 +0300

    @reduxjs/toolkit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,73 @@
+import {combineReducers} from "redux";
+
 let nextTodoId = 0
-export const addTodo = text => ({
-    type: 'ADD_TODO',
-    id: nextTodoId++,
-    text
+// export const addTodo = text => ({
+//     type: 'ADD_TODO',
+//     id: nextTodoId++,
+//     text
+// })
+//
+// export const setVisibilityFilter = filter => ({
+//     type: 'SET_VISIBILITY_FILTER',
+//     filter
+// })
+//
+// export const toggleTodo = id => ({
+//     type: 'TOGGLE_TODO',
+//     id
+// })
+
+
+import {createSlice} from "@reduxjs/toolkit";
+import {VisibilityFilters} from "../constants";
+
+const todo = createSlice({
+    name: "todo",
+    initialState: [],
+    reducers: {
+        addTodo(state, action) {
+            const text = action.payload;
+
+            state.push({
+                id: nextTodoId++,
+                text: text,
+                completed: false
+            });
+        },
+        toggleTodo(state, action) {
+            const id = action.payload;
+
+            state.forEach(todo => {
+                    if (todo.id === id) {
+                        todo.completed = !todo.completed
+                    }
+                }
+            )
+        }
+    }
+});
+
+const visibilityFilter = createSlice({
+    name: "visibilityFilter",
+    initialState: VisibilityFilters.SHOW_ALL,
+    reducers: {
+        setVisibilityFilter(state, action) {
+            return  action.payload;
+        },
+    }
+});
+
+
+const {actions: acrionsTodo, reducer: todoReducer} = todo;
+export const {addTodo, toggleTodo} = acrionsTodo;
+
+const {actions: actionsVisibilityFilter, reducer: reducerVisibilityFilter} = visibilityFilter;
+export const {setVisibilityFilter} = actionsVisibilityFilter;
+
+
+export default combineReducers({
+    todos: todoReducer,
+    visibilityFilter: reducerVisibilityFilter
 })
 
-export const setVisibilityFilter = filter => ({
-    type: 'SET_VISIBILITY_FILTER',
-    filter
-})
-
-export const toggleTodo = id => ({
-    type: 'TOGGLE_TODO',
-    id
-})
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import { render } from 'react-dom'
 import {compose, createStore} from 'redux'
-import { Provider } from 'react-redux'
+import { Provider, applyMiddleware } from 'react-redux'
 import App from './components/App'
-import rootReducer from './reducers'
+// import rootReducer from './reducers'
 import DevTools from './containers/DevTools';
 import { composeWithDevTools } from 'redux-devtools-extension';
-
 const enhancer = compose(
     // DevTools.instrument(),
     composeWithDevTools()
 );
 
-const store = createStore(rootReducer, enhancer)
+//toolkit
+import rootReducer from './actions'
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
+const store = configureStore({
+    reducer: rootReducer,
+    // middleware: [loggerMiddleware, ...getDefaultMiddleware()],
+    // preloadedState,
+    // enhancers: [monitorReducersEnhancer]
+})
+
+
+// const store = createStore(rootReducer, enhancer)
 
 render(
     <Provider store={store}>
